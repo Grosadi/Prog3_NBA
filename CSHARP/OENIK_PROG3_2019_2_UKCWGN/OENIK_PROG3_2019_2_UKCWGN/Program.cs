992f8f5b65ea2c7b06a92642f70190ce7b3e447e
grosadi <groska.adam98@gmail.com>
2019-10-07 12:06:55 +0200

    add full C# sln

---
+++ CSHARP/OENIK_PROG3_2019_2_UKCWGN/OENIK_PROG3_2019_2_UKCWGN/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OENIK_PROG3_2019_2_UKCWGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
