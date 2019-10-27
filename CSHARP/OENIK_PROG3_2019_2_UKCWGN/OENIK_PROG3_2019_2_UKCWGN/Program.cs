62f8773b4f195107d66f0a4e5357ca0aa008baab
grosadi <groska.adam98@gmail.com>
2019-10-27 10:55:36 +0100

    modify MENU

--- CSHARP/OENIK_PROG3_2019_2_UKCWGN/OENIK_PROG3_2019_2_UKCWGN/Program.cs
+++ CSHARP/OENIK_PROG3_2019_2_UKCWGN/OENIK_PROG3_2019_2_UKCWGN/Program.cs
@@ -21,41 +21,115 @@ namespace OENIK_PROG3_2019_2_UKCWGN
         {
             Console.Clear();
             Console.WriteLine("Choose an option:");
-            Console.WriteLine("1) List Teams");
-            Console.WriteLine("2) Add new Team");
-            Console.WriteLine("3) Modify Team");
-            Console.WriteLine("4) Delete Team");
-            Console.WriteLine("5) List Players");
-            Console.WriteLine("6) Add new Player");
-            Console.WriteLine("7) Modify Player");
-            Console.WriteLine("8) Delete Player");
-            Console.WriteLine("10) List Coaches");
-            Console.WriteLine("11) Add new Coach");
-            Console.WriteLine("12) Modify Coach");
-            Console.WriteLine("13) Delete Coach");
-            Console.WriteLine("14) List Player and Coaches of a Team");
-            Console.WriteLine("14) Modify a Team Roster");
-            Console.WriteLine("15) Ranking Teams by win percentage");
-            Console.WriteLine("16) Has this Team ever been a champion?");
-            Console.WriteLine("17) Has this Player ever been a champion?");
-            Console.WriteLine("18) Has this Coach ever been a champion?");
-            Console.WriteLine("19) List of Teams and order by Value of Roster");
-            Console.WriteLine("20) Get season standing from Web Application");
-            Console.WriteLine("21) Exit");
+            Console.WriteLine("1) Teams");
+            Console.WriteLine("2) Players");
+            Console.WriteLine("3) Coaches");
+            Console.WriteLine("4) List Player and Coaches of a Team");
+            Console.WriteLine("5) Modify a Team Roster");
+            Console.WriteLine("6) Ranking Teams by win percentage");
+            Console.WriteLine("7) Has this Team ever been a champion?");
+            Console.WriteLine("8) Has this Player ever been a champion?");
+            Console.WriteLine("9) Has this Coach ever been a champion?");
+            Console.WriteLine("10) List of Teams and order by Value of Roster");
+            Console.WriteLine("11) Get season standing from Web Application");
+            Console.WriteLine("12) Exit");
             Console.Write("\r\nSelect an option: ");
 
             switch (Console.ReadLine())
             {
                 case "1":
-                    Console.WriteLine("Ez a funkció még nem elérhető");
-                    Console.WriteLine("Press Enter to get back to the MENU");
-                    Console.ReadLine();
+                    Console.WriteLine("1) List Teams");
+                    Console.WriteLine("2) Add Team");
+                    Console.WriteLine("3) Modify Team");
+                    Console.WriteLine("4) Remove Team");
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            Console.WriteLine("Ez a funkció még nem elérhető");
+                            Console.WriteLine("Press Enter to get back to the MENU");
+                            Console.ReadLine();
+                            return true;
+                        case "2":
+                            Console.WriteLine("Ez a funkció még nem elérhető");
+                            Console.WriteLine("Press Enter to get back to the MENU");
+                            Console.ReadLine();
+                            return true;
+                        case "3":
+                            Console.WriteLine("Ez a funkció még nem elérhető");
+                            Console.WriteLine("Press Enter to get back to the MENU");
+                            Console.ReadLine();
+                            return true;
+                        case "4":
+                            Console.WriteLine("Ez a funkció még nem elérhető");
+                            Console.WriteLine("Press Enter to get back to the MENU");
+                            Console.ReadLine();
+                            return true;
+                    }
+
                     return true;
+
                 case "2":
-                    Console.WriteLine("Ez a funkció még nem elérhető");
-                    Console.WriteLine("Press Enter to get back to the MENU");
-                    Console.ReadLine();
+                    Console.WriteLine("1) List Players");
+                    Console.WriteLine("2) Add Player");
+                    Console.WriteLine("3) Modify Player");
+                    Console.WriteLine("4) Remove Player");
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            Console.WriteLine("Ez a funkció még nem elérhető");
+                            Console.WriteLine("Press Enter to get back to the MENU");
+                            Console.ReadLine();
+                            return true;
+                        case "2":
+                            Console.WriteLine("Ez a funkció még nem elérhető");
+                            Console.WriteLine("Press Enter to get back to the MENU");
+                            Console.ReadLine();
+                            return true;
+                        case "3":
+                            Console.WriteLine("Ez a funkció még nem elérhető");
+                            Console.WriteLine("Press Enter to get back to the MENU");
+                            Console.ReadLine();
+                            return true;
+                        case "4":
+                            Console.WriteLine("Ez a funkció még nem elérhető");
+                            Console.WriteLine("Press Enter to get back to the MENU");
+                            Console.ReadLine();
+                            return true;
+                    }
+
                     return true;
+
+                case "3":
+                    Console.WriteLine("1) List Coaches");
+                    Console.WriteLine("2) Add Coach");
+                    Console.WriteLine("3) Modify Coach");
+                    Console.WriteLine("4) Remove Coach");
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            Console.WriteLine("Ez a funkció még nem elérhető");
+                            Console.WriteLine("Press Enter to get back to the MENU");
+                            Console.ReadLine();
+                            return true;
+                        case "2":
+                            Console.WriteLine("Ez a funkció még nem elérhető");
+                            Console.WriteLine("Press Enter to get back to the MENU");
+                            Console.ReadLine();
+                            return true;
+                        case "3":
+                            Console.WriteLine("Ez a funkció még nem elérhető");
+                            Console.WriteLine("Press Enter to get back to the MENU");
+                            Console.ReadLine();
+                            return true;
+                        case "4":
+                            Console.WriteLine("Ez a funkció még nem elérhető");
+                            Console.WriteLine("Press Enter to get back to the MENU");
+                            Console.ReadLine();
+                            return true;
+                    }
+
+                    return true;
+
                 case "4":
                     Console.WriteLine("Ez a funkció még nem elérhető");
                     Console.WriteLine("Press Enter to get back to the MENU");
@@ -97,51 +171,6 @@ namespace OENIK_PROG3_2019_2_UKCWGN
                     Console.ReadLine();
                     return true;
                 case "12":
-                    Console.WriteLine("Ez a funkció még nem elérhető");
-                    Console.WriteLine("Press Enter to get back to the MENU");
-                    Console.ReadLine();
-                    return true;
-                case "13":
-                    Console.WriteLine("Ez a funkció még nem elérhető");
-                    Console.WriteLine("Press Enter to get back to the MENU");
-                    Console.ReadLine();
-                    return true;
-                case "14":
-                    Console.WriteLine("Ez a funkció még nem elérhető");
-                    Console.WriteLine("Press Enter to get back to the MENU");
-                    Console.ReadLine();
-                    return true;
-                case "15":
-                    Console.WriteLine("Ez a funkció még nem elérhető");
-                    Console.WriteLine("Press Enter to get back to the MENU");
-                    Console.ReadLine();
-                    return true;
-                case "16":
-                    Console.WriteLine("Ez a funkció még nem elérhető");
-                    Console.WriteLine("Press Enter to get back to the MENU");
-                    Console.ReadLine();
-                    return true;
-                case "17":
-                    Console.WriteLine("Ez a funkció még nem elérhető");
-                    Console.WriteLine("Press Enter to get back to the MENU");
-                    Console.ReadLine();
-                    return true;
-                case "18":
-                    Console.WriteLine("Ez a funkció még nem elérhető");
-                    Console.WriteLine("Press Enter to get back to the MENU");
-                    Console.ReadLine();
-                    return true;
-                case "19":
-                    Console.WriteLine("Ez a funkció még nem elérhető");
-                    Console.WriteLine("Press Enter to get back to the MENU");
-                    Console.ReadLine();
-                    return true;
-                case "20":
-                    Console.WriteLine("Ez a funkció még nem elérhető");
-                    Console.WriteLine("Press Enter to get back to the MENU");
-                    Console.ReadLine();
-                    return true;
-                case "21":
                     return false;
                 default:
                     return true;
