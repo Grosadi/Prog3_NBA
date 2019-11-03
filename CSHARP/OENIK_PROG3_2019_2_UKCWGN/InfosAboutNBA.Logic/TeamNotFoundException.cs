40ea366206a886d0d59ff1cf4aa95ae615778668
grosadi <groska.adam98@gmail.com>
2019-11-03 19:00:11 +0100

    Add Exceptions to TeamLogic

---
+++ CSHARP/OENIK_PROG3_2019_2_UKCWGN/InfosAboutNBA.Logic/TeamNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InfosAboutNBA.Logic
+{
+    public class TeamNotFoundException : ApplicationException
+    {
+        public string Msg { get; set; }
+
+        public TeamNotFoundException(string msg)
+        {
+            this.Msg = msg;
+        }
+    }
+}
