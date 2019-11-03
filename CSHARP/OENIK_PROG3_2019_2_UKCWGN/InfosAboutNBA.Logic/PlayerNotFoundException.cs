e3131221d7d7216f06c8f67e7d11ab92ed66a8b1
grosadi <groska.adam98@gmail.com>
2019-11-03 19:00:58 +0100

    Add exceptions to PlayerLogic

---
+++ CSHARP/OENIK_PROG3_2019_2_UKCWGN/InfosAboutNBA.Logic/PlayerNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InfosAboutNBA.Logic
+{
+    public class PlayerNotFoundException : ApplicationException
+    {
+        public string Msg { get; set; }
+
+        public PlayerNotFoundException(string msg)
+        {
+            this.Msg = msg;
+        }
+    }
+}
