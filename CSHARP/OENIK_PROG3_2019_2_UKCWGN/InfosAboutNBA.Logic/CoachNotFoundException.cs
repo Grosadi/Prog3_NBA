8b0e29ea7b7effdfd2766150646693bc76d192f6
grosadi <groska.adam98@gmail.com>
2019-11-03 19:00:35 +0100

    Add exception to CoachLogic

---
+++ CSHARP/OENIK_PROG3_2019_2_UKCWGN/InfosAboutNBA.Logic/CoachNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace InfosAboutNBA.Logic
+{
+    public class CoachNotFoundException : ApplicationException
+    {
+        public string Msg { get; set; }
+
+        public CoachNotFoundException(string msg)
+        {
+            this.Msg = msg;
+        }
+    }
+
+}
